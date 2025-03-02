2d699ad28b924bb876a0dff1175f8e848d05f98e
Rahim Thorm <sourabrahim@gmail.com>
2025-03-02 20:44:53 +0000

    Adding task scheduling with celery beat

--- module4/movienight/movienight/settings.py
+++ module4/movienight/movienight/settings.py
@@ -53,6 +53,7 @@ class Dev(Configuration):
             "rest_framework",
             "rest_framework.authtoken",
             'django_celery_results',
+            'django_celery_beat',
         ]
 
         MIDDLEWARE = [
--- module4/movienight/movies/schedule_setup.py
+++ module4/movienight/movies/schedule_setup.py
@@ -1,2 +1,16 @@
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+import json
+
 def schedule_setup():
-    pass
+    schedule, _ = IntervalSchedule.objects.get_or_create(
+      period=IntervalSchedule.MINUTES, every=1
+    )
+
+    args=json.dumps([])
+
+    PeriodicTask.objects.get_or_create(
+      name="Send schedule",
+      interval=schedule,
+      task="movies.tasks.notify_of_starting_soon",
+      args=args
+    )
